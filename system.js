2b7dba410c817a61643210fe747b8e49228abf04
Peter Elger <elger.peter@gmail.com>
2014-10-16 16:39:25 +0100

    corrected ids

--- system.js
+++ system.js
@@ -1,6 +1,6 @@
 exports.name = 'nscaledemo';
 exports.namespace = 'nscaledemo';
-exports.id = '3c1703f1-35bf-446c-875f-9bb50fc9302c';
+exports.id = 'e1144711-47bb-5931-9117-94f01dd20f6f';
 
 exports.topology = {
   local: {
