5b88b63394601e135603756a2b9e68912f953e27
Darragh Hayes <dara.hayes.1995@gmail.com>
2015-04-07 10:06:30 +0100

    using process containers

--- definitions/services.js
+++ definitions/services.js
@@ -4,11 +4,18 @@ exports.root = {
 
 exports.web = {
   type: 'docker',
+  override: {
+    process: {
+      type: 'process'
+    }
+  },
   specific: {
     repositoryUrl: 'git@github.com:nearform/nscaledemoweb.git',
+    processBuild: 'npm install',
     execute: {
       args: '-p 8000:8000 -d',
-      exec: ''
+      exec: '',
+      process: 'index.js'
     }
   }
 };
--- system.js
+++ system.js
@@ -5,6 +5,10 @@ exports.id = 'e1144711-47bb-5931-9117-94f01dd20f6f';
 exports.topology = {
   development: {
     root: ['web']
+  },
+
+  process: {
+    root: ['web']
   }
 };
 
